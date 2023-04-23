6041fbcbbd802d7854885a2b042e0f2a1bf9034f
luissndval <118476168+luissndval@users.noreply.github.com>
2023-04-23 13:15:23 -0300

    Update AltaOrdenPage.py

--- pages/AltaOrdenPage.py
+++ pages/AltaOrdenPage.py
@@ -1,6 +1,5 @@
 import time
 
-from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
